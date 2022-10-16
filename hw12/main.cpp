7012669db13b5b5e63f42dbfa2bb3a313b77cf1a
KenyNotKenny <66070478+Keny7503@users.noreply.github.com>
2022-10-16 21:27:02 +0700

    Update main.cpp

--- hw12/main.cpp
+++ hw12/main.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main() {
     int n,x,T=1, S=0, i=1;
-    cout<<"Insert n:";cin >> x >> n;
+    cout<<"Insert x:";cin >> x;
+    cout<<"Insert n:";cin >> n;
     while(i<=n){
         T*=x;
         S+=T;
